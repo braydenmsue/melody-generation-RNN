953fa212dbfb530d9292ac8f50a07e34e1ee40db
bms8 <bms8@sfu.ca>
2025-03-05 16:17:49 -0800

    data load

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
+import os
 
-    return
+
+def load_data(data_dir, set_name='train'):
+    file_path = os.path.join(data_dir, set_name + ".txt")
+    with open(file_path, "r") as f:
+        data = f.read()
+    return data     # string contents of txt file
+
+
+def main(input_dir):
+    data = load_data(input_dir)
+    print(type(data))
+    # print(data)
+
 
 if __name__ == "__main__":
-    main()
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(base_dir, 'data')
+    main(data_path)
+
